c7d359388b2856a7ad05a78e66b6e4b1b543132e
ovr1 <rybynok@mail.ru>
2019-04-10 01:15:56 +0300

    new

--- lesson5.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-n = int(len(set(Path(r'C:\Users\Oleg\PycharmProjects\pyweb-m4-hw2\lesson5.txt').read_text(encoding='utf-8').split())))
-print(n)
-
-l = int(len(set(Path(r'C:\Users\Oleg\PycharmProjects\pyweb-m4-hw2\lesson5.txt').read_text(encoding='utf-8').split(','))))
-print(l)
-
-print(round(l/n*100),'%')
