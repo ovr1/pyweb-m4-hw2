c3e2eca913016749fb44c2ce039e7dea62f8b038
ovr1 <rybynok@mail.ru>
2019-04-17 12:13:40 +0300

    test

--- server.py
+++ server.py
@@ -62,4 +62,8 @@ run(
   port=8080,
   autoreload=True
 )
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
 
