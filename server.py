34a64afeecde6dca8a726638ebb94113f09a0e88
Lenovo <rybynok@mail.ru>
2019-08-27 21:56:05 +0300

    test

--- server.py
+++ server.py
@@ -57,7 +57,11 @@ def index():
 def api_test():
     return {"test_passed": True}
 
-
+run(
+  host="localhost",
+  port=8080,
+  autoreload=True
+)
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
